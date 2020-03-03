b6eba65d8fd2815f591ef685afb6461b2efa7f50
wjamieson3 <60943356+wjamieson3@users.noreply.github.com>
2020-03-03 10:54:23 -0500

    Create Test.java

---
+++ ThreadStateDemo/Test.java
@@ -0,0 +1,51 @@
+package javaThreadDemo;
+
+
+public class Test implements Runnable {
+
+	public static Thread thread1;
+	public static Test obj;
+
+	public static void main(String[] args) {
+		obj = new Test();
+		thread1 = new Thread(obj);
+
+		// thread1 created and is currently in the NEW state.
+		System.out.println("State of thread1 after creating it - " + thread1.getState());
+		thread1.start();
+
+		// thread1 moved to Runnable state
+		System.out.println("State of thread1 after calling .start() method on it - " + thread1.getState());
+	}
+
+	public void run() { // inside thread1
+
+		thread myThread = new thread();
+		Thread thread2 = new Thread(myThread);
+
+		// thread1 created and is currently in the NEW state.
+		System.out.println("State of thread2 after creating it - " + thread2.getState());
+		thread2.start();
+
+		// thread2 moved to Runnable state
+		System.out.println("State of thread2 after calling .start() method on it - " + thread2.getState());
+
+		// moving thread2 to timed waiting state
+		try {
+			// moving thread2 to timed waiting state
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("State of thread2 after calling .sleep() method on it - " + thread2.getState());
+
+		try {
+			// makes the current thread1 wait for thread2 to die
+			thread2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("State of thread2 when it has finished it's execution - " + thread2.getState());
+	}
+
+}
